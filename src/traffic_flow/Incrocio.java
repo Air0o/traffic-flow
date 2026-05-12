60fdb0bce6a083c5d59472e9838f6376b11f77af
LU.COELI <LU.COELI@sisinfo21new.magistri.local>
2026-05-12 10:24:48 +0200

    Scrittura base Incrocio

--- src/traffic_flow/Incrocio.java
+++ src/traffic_flow/Incrocio.java
@@ -2,10 +2,21 @@
  */
 package traffic_flow;
 
+import veicoli.Veicolo;
+
 /**
  *
  * @author GI.AIROLDI
  */
 public class Incrocio {
+    private boolean corsiaNSLibera = false;
+    private boolean corsiaEOLibera = false;
     
+    public synchronized void transitaCorsiaNS(Veicolo v) throws InterruptedException{
+       
+    }
+    
+    public synchronized void transitaCorsiaEO(Veicolo v) throws InterruptedException{
+        
+    }
 }
