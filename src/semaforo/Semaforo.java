a28c5234df40ddda49b118c523abf1357fd69f9b
lcoel <lcoel@DESKTOP-COELI>
2026-05-13 15:31:12 +0200

    Classe Semaforo: Inizializzazione cambiaFase()

--- src/semaforo/Semaforo.java
+++ src/semaforo/Semaforo.java
@@ -18,5 +18,20 @@ public class Semaforo {
 
     public synchronized void cambiaFase(){
         //rosso > verde > giallo > rosso
+        switch(faseAttuale){
+            case rosso:
+                faseAttuale = FaseSemaforo.verde;
+                break;
+            case giallo:
+                faseAttuale = FaseSemaforo.rosso;
+                break;
+            case verde:
+                faseAttuale = FaseSemaforo.giallo;
+                break;
+            default:
+                faseAttuale = FaseSemaforo.rosso;
+                break;
+        }
+        notifyAll();
     }
 }
