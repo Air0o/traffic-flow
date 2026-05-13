a0b61061f7757fcd5c63061e1f4f9d4d865e630e
G1ultag <giulio.airoldi09@gmail.com>
2026-05-13 15:02:20 +0200

    Convertito switch in rule switch e creato classe Semaforo base

--- src/semaforo/Semaforo.java
+++ src/semaforo/Semaforo.java
@@ -7,5 +7,16 @@ package semaforo;
  * @author GI.AIROLDI
  */
 public class Semaforo {
+    private FaseSemaforo faseAttuale;
     
+
+    public synchronized void attendi(FaseSemaforo faseDiPassaggio) throws InterruptedException{
+        while(faseAttuale != faseDiPassaggio){
+            wait();
+        }
+    }
+
+    public synchronized void cambiaFase(){
+        //rosso > verde > giallo > rosso
+    }
 }
